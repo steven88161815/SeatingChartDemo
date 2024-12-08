3a0c4f42ff0f564d6f536d2fbf47140f6245b8be
李品陞 <93220829+steven88161815@users.noreply.github.com>
2024-12-09 03:09:31 +0800

    Delete seating directory

--- seating/src/main/java/com/example/seating/mapper/EmployeeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.seating.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.seating.entity.Employee;
-
-
-public interface EmployeeMapper extends BaseMapper<Employee> {
-    // 可以新增自定義的查詢方法
-}
